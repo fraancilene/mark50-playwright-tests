b33c6b3c6ed55254ce65bac920b165739826efd0
Francilene Silva <francilenesilva.fps10@gmail.com>
2026-04-27 11:47:53 -0300

    config: correções

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,7 +1,12 @@
 import { test, expect } from "@playwright/test";
+import { TasksPage } from "./support/pages/tasks";
+
+let tasksPage: TasksPage;
+test.beforeEach(({ page }) => {
+  tasksPage = new TasksPage(page);
+});
 
 test("webapp deve estar online", async ({ page }) => {
-  await page.goto("http://localhost:3000");
+  await tasksPage.go();
   await expect(page).toHaveTitle("Gerencie suas tarefas com Mark L");
-  //await page.waitForTimeout(3000)
 });
